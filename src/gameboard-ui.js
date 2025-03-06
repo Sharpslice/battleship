eab2b05b9da407b7ae2c3bb9ee75d48691a17f87
sharpslice <davidlein1440p@gmail.com>
2025-03-06 13:41:54 -0800

    added second board and updated ui

--- src/gameboard-ui.js
+++ src/gameboard-ui.js
@@ -3,13 +3,14 @@ export class GameboardUi{
     constructor(gameboard){
         this.gameboard = gameboard
         this.size = gameboard.size;
-        
     }
     renderBoard(){
         const mainContainer = document.getElementById("gameboard");
         if(!mainContainer){
             console.error("main div not found!")
         }
+        const board = document.createElement("div");
+        board.className ="board"
         for(let i =0; i < this.size;i++)
         {
             for(let j = 0; j< this.size;j++)
@@ -19,21 +20,22 @@ export class GameboardUi{
                 const squareDiv = document.createElement("div");
                 squareDiv.classList.add("board-square");
                 squareDiv.textContent = `(${i},${j})`;
-                
-               
+                squareDiv.width = "100px";
+                squareDiv.height = "100px";
 
-    
+
                 if(this.gameboard.placedCoords.some(([row,col])=> row === i && col === j)){
                     squareDiv.style.backgroundColor ="green"
                 }
                 let coords = [i,j]
                 squareDiv.addEventListener("click",(event)=> this.handleClick(event,squareDiv,coords));
 
-                mainContainer.append(squareDiv);
+                board.append(squareDiv);
             }
             
 
         }
+        mainContainer.append(board);
     }
     handleClick(event,squareDiv,coords){
         const [i,j] = coords
--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function main(){
 
 
     player1Ui.renderBoard();
-
+    player2Ui.renderBoard();
    
 
 }
